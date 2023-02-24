6ffe584387294e3d8339e788be7715c87153d09b
u6513115 <122536267+u6513115@users.noreply.github.com>
2023-02-24 07:17:09 +0000

    make enqueue dequeue

--- Node.h
+++ Node.h
@@ -4,6 +4,9 @@
 //
 //  Created by Mingmanas Sivaraksa on 5/2/2566 BE.
 //
+#include <stdio.h>
+#include <stdlib.h>
+
 #ifndef Node_h
 #define Node_h
 
@@ -21,6 +24,12 @@ void enqueue(NodePtr * head, NodePtr* tail, int x){
   Node* new_node=(NodePtr) malloc(sizeof(Node));
 if(new_node){ 
     /* Finish queue*/
+  new_node->data = x;
+  new_node->nextPtr = NULL;
+  if(*head==NULL)
+    *head = new_node;
+  else
+    (*tail)->nextPtr = new_node;
  }
 }
 
@@ -30,6 +39,10 @@ int dequeue(NodePtr* head, NodePtr* tail){
    if(t){
    int value= t->data;
    /* Finish dequeue*/
+     *head = t->nextPtr;
+     if(*head==NULL)
+       *tail=NULL;
+     free(t);
        
        
    return value;
