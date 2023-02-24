0af2d403f7c09c500de904845ae95766ddee8c50
u6513115 <122536267+u6513115@users.noreply.github.com>
2023-02-24 07:51:01 +0000

    add enqueue_struct dequeue_struct
    change main.c  to struct

--- Queue.h
+++ Queue.h
@@ -10,15 +10,30 @@ void enqueue_struct(Queue* q, int x){
   Node *new_node=(Node*) malloc(sizeof(Node));
 if(new_node){ 
   /*Finish enqueue */
+  new_node->data=x; //เอา Node มาจาก Node.h
+  new_node->nextPtr=NULL;
+  if(q->headPtr==NULL)
+    q->headPtr = new_node;
+  else
+    q->tailPtr->nextPtr=new_node;
+  q->tailPtr = new_node;
+  q->size++;
  }
 }
 
 
 int dequeue_struct(Queue *q){
    NodePtr t=q->headPtr;
-   if(t){
-   int value= t->data;
-       /*Finish dequeue */
+   if(q->size>0){
+     int value= t->data;
+     q->headPtr=t->nextPtr;
+     if(q->headPtr==NULL)
+     {
+       q->tailPtr==NULL;
+     }
+    q->size--;
+    free(t);
+         /*Finish dequeue */
    return value;
    }
    printf("Empty queue");
